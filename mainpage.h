f1fcea166ebae7f467d743783b105933da83f419
BenjaminChenTW <s910190benjamin@gmail.com>
2015-05-31 23:49:40 +0800

    Change1

--- mainpage.h
+++ mainpage.h
@@ -2,6 +2,8 @@
 #define MAINPAGE_H
 
 #include <QWidget>
+#include <QKeyEvent>
+#include "game.h"
 
 namespace Ui {
 class MainPage;
@@ -12,11 +14,28 @@ class MainPage : public QWidget
     Q_OBJECT
 
 public:
-    explicit MainPage(QWidget *parent = 0);
+    explicit MainPage(QWidget *parent, int *Win);
     ~MainPage();
+    void dirEvent(QKeyEvent *event);
+
+private slots:
+    void Quit();
+    void Back();
 
 private:
     Ui::MainPage *ui;
+    void firstEvent();
+    void changeValue(int X, int Y, int value);
+    void addNew();
+    void endPage(int result);
+    bool ifEnd();
+    bool upEvent();
+    bool downEvent();
+    bool leftEvent();
+    bool rightEvent();
+    Game *blocks[4][4];
+    bool ifEmpty[4][4];
+    int *win;
 };
 
 #endif // MAINPAGE_H
